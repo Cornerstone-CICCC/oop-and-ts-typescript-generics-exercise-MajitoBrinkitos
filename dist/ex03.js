9340e47dee477bc678137f576920a1434f9b3853
Majito Brinkitos <mcvef2013@gmail.com>
2025-02-11 09:42:27 -0800

    ex03

--- dist/ex03.js
+++ dist/ex03.js
@@ -2,5 +2,7 @@
 // 3. Create a generic arrow function called `createPair` that accepts two arguments of different types
 // and returns them as a tuple/array. 
 // Pass `true` and `42` as arguments and log the result.
-const createPair = (first, second) => [first, second];
+const createPair = (first, second) => {
+    return [first, second];
+};
 console.log(createPair(true, 42)); // Expected output: [true, 42]
