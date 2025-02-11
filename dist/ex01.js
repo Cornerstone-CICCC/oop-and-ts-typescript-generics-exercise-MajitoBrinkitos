94dead0c016748dd0ccdcb5d04e0dade7fed3305
Majito Brinkitos <mcvef2013@gmail.com>
2025-02-11 09:11:11 -0800

    ex01.ts

--- dist/ex01.js
+++ dist/ex01.js
@@ -1,9 +1,8 @@
+"use strict";
 // 1. Create a generic arrow function called `getFirstElement` that accepts an array of any type 
 // and returns the first element of the array. 
 // Pass an array of strings ["Hello", "World"] and log the result.
-
-const getFirstElement = <T>(arr: T): T => {
+const getFirstElement = (arr) => {
     return arr[0];
-}
-
+};
 console.log(getFirstElement(["Hello", "World"])); // Expected output: "Hello"
